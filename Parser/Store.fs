2f290c8ab0ac8f5bf6e184e1adb024d711dec631
Zlatozar Zhelyazkov <z_zhelyazkov@prosyst.bg>
2018-07-30 17:49:12 +0300

    Add store using symbol index as a key

--- Parser/Store.fs
+++ Parser/Store.fs
@@ -9,7 +9,7 @@ type Symbol = string * int
 
 let private nextSym = ref 0
 
-let vars = new Dictionary<string, int>()
+let private vars = new Dictionary<string, int>()
 
 let symbol name =
     let found, value = vars.TryGetValue name
@@ -19,7 +19,17 @@ let symbol name =
          vars.Add (name, idx)
          (name, idx)
 
-let name (sym, idx) = sym
+let name (name, (_: int)) = name
+
+let showSymbols = Seq.iter (printf "%A\n") vars
 
 // _____________________________________________________________________________
-//                                                                Generic store
+//                                         Symbol index as a key (optimization)
+
+type Table<'a> = Map<int, 'a>
+
+let empty = Map.empty<int, 'a>
+
+let enter (table: Table<'a>) (symbol: Symbol) value = table.Add ((snd symbol), value)
+
+let lookup (table: Table<'a>) (symbol: Symbol) = table.Item (snd symbol)
