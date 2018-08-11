94aba9cae7a8725c831cb9429458325fba98c816
Zlatozar Zhelyazkov <z_zhelyazkov@prosyst.bg>
2018-08-11 16:58:29 +0300

    Handle duplicates - type, params, func, rec fields

--- SemanticAnalysis/TigerSemantic.fs
+++ SemanticAnalysis/TigerSemantic.fs
@@ -81,6 +81,13 @@ let checkSame (ty1, ty2, pos) =
     | (_, NAME _)  -> ()
     | _            -> checkBothEq (ty1, ty2, pos)
 
+let rec checkDup ((decNameList: Store.Symbol list), positions) =
+    match (decNameList, positions) with
+    | (name::rest, pos::poss) -> if (List.exists (fun x -> name = x) rest)
+                                     then error pos (sprintf "duplicated definition `%s`." (Store.name name))
+                                     else checkDup(rest, poss)
+    | (_, _)                  -> ()
+
 // (NAME a, (NAME b, (NAME c, ref T))), (NAME, nil)  or could be empty
 let rec actualTy (tenv, ty: Ty) =
     match ty with
@@ -114,15 +121,15 @@ let rec transVar ((venv: VEnv), fenv, tenv, breakpoint, (var: Absyn.TVar)) :ExpT
                             let varTy = transVar (venv, fenv, tenv, breakpoint, tVar)
 
                             let recordType = actualTy (tenv, varTy.ty)
-                            match recordType with 
-                            | RECORD (fieldTys, _) -> let rec findField record fieldList = 
+                            match recordType with
+                            | RECORD (fieldTys, _) -> let rec findField record fieldList =
                                                           match fieldList with
                                                           | []           -> error pos (sprintf "field `%s` is not a member of that record type." (Store.name record))
                                                                             { exp=(); ty=recordType }
-                                                          | (s, t)::rest -> if (s = record) 
+                                                          | (s, t)::rest -> if (s = record)
                                                                             then { exp=(); ty=t }
                                                                             else findField record rest
-                                                       
+
                                                       findField sym fieldTys
 
                             | _                    -> error pos "expecting a record variable."
@@ -139,7 +146,7 @@ let rec transVar ((venv: VEnv), fenv, tenv, breakpoint, (var: Absyn.TVar)) :ExpT
                             | ARRAY(ty', _) -> { exp=(); ty=ty' }
                             | _             -> error pos "expecting an array variable."
                                                dummyTransExp
- 
+
 and transExp ((venv: VEnv), (fenv: FEnv), (tenv: TEnv), (breakpoint: BreakPoint), (exp: Absyn.TExp)) :ExpTy =
     match exp with
     | IntExp i           -> printfn "        !IntExp"
@@ -186,10 +193,10 @@ and transExp ((venv: VEnv), (fenv: FEnv), (tenv: TEnv), (breakpoint: BreakPoint)
                             | Some funEntry -> let actualParmL = List.length callRec.args
                                                let formalParmL = List.length funEntry.formals
 
-                                               if actualParmL <> formalParmL 
+                                               if actualParmL <> formalParmL
                                                    then error callRec.pos (sprintf "wrong number of arguments. Expecting %i agruments but given %i." formalParmL actualParmL)
                                                         { exp=(); ty=funEntry.result }
-                                                   else 
+                                                   else
                                                         let checkSame (e, formalTy) =
                                                             let expTy = transExp (venv, fenv, tenv, breakpoint, e)
                                                             checkSame (actualTy (tenv, expTy.ty), actualTy (tenv, formalTy), callRec.pos)
@@ -206,24 +213,24 @@ and transExp ((venv: VEnv), (fenv: FEnv), (tenv: TEnv), (breakpoint: BreakPoint)
                                             | (RECORD (fieldTys, _)) -> let rectTysL = List.length recRec.fields
                                                                         let fieldTysL = List.length fieldTys
                                                                         if rectTysL <> fieldTysL
-                                                                            then 
+                                                                            then
                                                                                  error recRec.pos (sprintf "expecting %i fields but given %i."  fieldTysL rectTysL)
                                                                                  { exp=(); ty=NIL }
-                                                                            else 
+                                                                            else
                                                                                  let checkField ((sym, e, p), (tySym, ty)) =
                                                                                      // order of fields must match
-                                                                                     if sym = tySym 
+                                                                                     if sym = tySym
                                                                                          then
                                                                                              let expTy = transExp (venv, fenv, tenv, breakpoint, e)
                                                                                              checkSame (expTy.ty, ty, p)
-                                                                                         else 
+                                                                                         else
                                                                                              error p (sprintf "expecting `%s`, given `%s`." (Store.name tySym) (Store.name sym))
 
                                                                                  List.iter checkField (List.zip recRec.fields fieldTys)
                                                                                  { exp=(); ty=recTy }
 
                                             | _                      -> error recRec.pos "expecting a record type."
-                                                                        { exp=(); ty=NIL } 
+                                                                        { exp=(); ty=NIL }
 
     | ArrayExp arrayRec  -> printfn "    !ArrayExp"
                             match Store.lookup (tenv, arrayRec.typ) with
@@ -249,7 +256,7 @@ and transExp ((venv: VEnv), (fenv: FEnv), (tenv: TEnv), (breakpoint: BreakPoint)
 
     | IfExp ifRec        -> printfn "    !IfExp"
                             let test = transExp (venv, fenv, tenv, breakpoint, ifRec.test)
-                            if test.ty = INT then () 
+                            if test.ty = INT then ()
                                 else error ifRec.pos "test clause in `if/then/(else)` expression should be an integer."
 
                             let then' = transExp (venv, fenv, tenv, breakpoint, ifRec.then')
@@ -261,7 +268,7 @@ and transExp ((venv: VEnv), (fenv: FEnv), (tenv: TEnv), (breakpoint: BreakPoint)
                             | None _ -> if then'.ty = UNIT then ()
                                             else error ifRec.pos "`if/then` expression does not return a value."
                                         { exp=(); ty=UNIT }
- 
+
     // 'break' should know that it is in a cycle
     | WhileExp whileRec  -> printfn "    !WhileExp"
                             let test = transExp (venv, fenv, tenv,  breakpoint, whileRec.test)
@@ -315,7 +322,7 @@ and transDec (venv, fenv, tenv, breakpoint, (dec: Absyn.TDec)) :ProgEnv =
                             let tenv'' = List.fold inferDeclTypes tenv' typeRecList
 
                             let rec inCycle (seen, tyRef, pos) =
-                                match tyRef with 
+                                match tyRef with
                                 | None   -> error pos "can't find type definition."
                                             true
                                 | Some t -> match t with
@@ -323,7 +330,7 @@ and transDec (venv, fenv, tenv, breakpoint, (dec: Absyn.TDec)) :ProgEnv =
                                                                   then true
                                                                   else inCycle (s2::seen, !tr, pos)
                                             | _             -> false
-                                          
+
                             let rec checkEach (typeRecList: TypeDecRec list) =
                                 match typeRecList with
                                 | []       -> ()
@@ -335,6 +342,11 @@ and transDec (venv, fenv, tenv, breakpoint, (dec: Absyn.TDec)) :ProgEnv =
 
                             // 3. Check for cyclic NAME definitions
                             checkEach (typeRecList)
+
+                            // 4. Should not have duplicate types
+                            let allTypeNames = List.map (fun (x: TypeDecRec) -> x.name) typeRecList
+                            let allTypePos =  List.map (fun (x: TypeDecRec) -> x.pos) typeRecList
+                            checkDup (allTypeNames, allTypePos)
 
                             { venv=venv; fenv=fenv; tenv=tenv'' }
 
@@ -367,22 +379,22 @@ and transDec (venv, fenv, tenv, breakpoint, (dec: Absyn.TDec)) :ProgEnv =
                             let transFun (venv, fenv, tenv, breakpoint, (funDecRec: FunDecRec), (funEntry: FunEntry)) =
 
                                 let getParams = List.map (transParam tenv) funDecRec.param
-                                                         
-                                let addParam (venv': VEnv) paramEntry = 
-                                    Store.enter (venv', paramEntry.name, { ty=paramEntry.ty; access=() }) 
 
-                                match funDecRec.result with 
+                                let addParam (venv': VEnv) paramEntry =
+                                    Store.enter (venv', paramEntry.name, { ty=paramEntry.ty; access=() })
+
+                                match funDecRec.result with
                                 | Some (result, resultPos) -> match Store.lookup (tenv, result) with
                                                               | None          -> error resultPos "result type of the function is undefined."
                                                               | Some resultTy -> let expResult =
                                                                                      transExp ((List.fold addParam venv getParams), fenv, tenv, breakpoint, funDecRec.body)
-             
+
                                                                                  checkSame (resultTy, expResult.ty, resultPos)
                                                                                  // Traslation(using funEntry) should be returned in next phase
-                                
+
                                 | None                     -> transExp ((List.fold addParam venv getParams), fenv, tenv, breakpoint, funDecRec.body) |> ignore
                                                               // Traslation(using funEntry) should be returned in next phase
-                            
+
                             // Return the type (sum of types of the parameters) of the funcition head
                             let functionHeader (tenv, funDecRec) :FunEntry =
                                 let paramsTy = List.map (fun p -> (transParam tenv p).ty) funDecRec.param
@@ -391,21 +403,30 @@ and transDec (venv, fenv, tenv, breakpoint, (dec: Absyn.TDec)) :ProgEnv =
                                 | Some (sym, pos) -> match Store.lookup (tenv, sym) with
                                                      | None          -> error pos "undefined type."
                                                                         { formals=paramsTy; result=UNIT }
-                                              
+
                                                      | Some resultTy -> { formals=paramsTy; result=resultTy }
-                                              
+
                                 | None            -> { formals=paramsTy; result=UNIT }
 
-                            // 1. Add function header first                         
-                            let fenv' = 
-                                List.fold (fun fenv (funDecRec: FunDecRec) 
+                            // 1. Add function header first
+                            let fenv' =
+                                List.fold (fun fenv (funDecRec: FunDecRec)
                                                     -> Store.enter (fenv, funDecRec.name, functionHeader (tenv, funDecRec))) fenv funDecRecList
 
-                            // 2. Then process functions bodies
-                            let checkFunDec (funDecRec: FunDecRec) = match Store.lookup (fenv', funDecRec.name) with
-                                                                     | None          -> error funDecRec.pos "Tiger Semantic Analysis did not find function header."
+                            // 2. Then process functions bodies and watchout for duplicate paramter names
+                            let checkFunDec (funDecRec: FunDecRec) = let allParmNames = List.map (fun (x: FieldRec) -> x.name) funDecRec.param
+                                                                     let allParamPos =  List.map (fun (x: FieldRec) -> x.pos) funDecRec.param
+                                                                     checkDup (allParmNames, allParamPos)
+
+                                                                     match Store.lookup (fenv', funDecRec.name) with
+                                                                     | None          -> error funDecRec.pos "Tiger Semantic Analysis did not find function head."
                                                                      | Some funEntry -> transFun (venv, fenv', tenv, breakpoint, funDecRec, funEntry)
-                            
+
+                            // 3. Should not contain duplicate functions name
+                            let allFuncNames = List.map (fun (x: FunDecRec) -> x.name) funDecRecList
+                            let allFuncPos = List.map (fun (x: FunDecRec) -> x.pos) funDecRecList
+                            checkDup (allFuncNames, allFuncPos)
+
                             // Start checking function declaration
                             List.iter checkFunDec funDecRecList
                             { venv=venv; fenv=fenv'; tenv=tenv }
@@ -420,8 +441,12 @@ and transTy (tenv, (ty: Absyn.TType)) :Ty =
 
     | RecordTy fieldRecList
                          -> printfn "          !RecordTy"
+                            let allFieldNames = List.map (fun (x: FieldRec) -> x.name) fieldRecList
+                            let allFieldPos = List.map (fun (x: FieldRec) -> x.pos) fieldRecList
+                            checkDup (allFieldNames, allFieldPos)
+
                             let getFieldType (fieldRec: FieldRec) =
-                                match Store.lookup (tenv, fieldRec.typ) with 
+                                match Store.lookup (tenv, fieldRec.typ) with
                                 | None         -> error fieldRec.pos (sprintf "type `%s` is not defined." (Store.name fieldRec.typ))
                                                   None
                                 | Some symType -> Some (fieldRec.name, symType)
@@ -434,4 +459,4 @@ and transTy (tenv, (ty: Absyn.TType)) :Ty =
                             match Store.lookup (tenv, sym) with
                             | None ->          error pos (sprintf "type `%s` is not defined." (Store.name sym))
                                                NIL
-                            | Some symType ->  ARRAY (symType, ref ())        
+                            | Some symType ->  ARRAY (symType, ref ())
