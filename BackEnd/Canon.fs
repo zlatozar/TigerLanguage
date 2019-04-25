7e8729b4fb85c623631710280ead3ed6f27e935a
Zlatozar Zhelyazkov <zlatozar@gmail.com>
2019-04-25 14:39:07 +0300

    Add comments

--- BackEnd/Canon.fs
+++ BackEnd/Canon.fs
@@ -4,67 +4,61 @@ open Tree
 
 // Olin Shivers' version
 
-(*
- * 1. Eliminate SEQ & EXP from the statement language.
- * 2. Allow calls only as the right-hand source of a MOVE stmt or
- *    as a top-level call that doesn't produce a value. That is, calls come
- *    up to top-level -- they are not part of the exp' language, so can't
- *    appear way down inside some expression.
- *
- * A list of these things looks much more like "generic" assembler:
- *
- *    FACT:				            // LABEL'
- *      if n=0 then L1 else L2		// CJUMP'
- *    L1:				            // LABEL'
- *      rv := 1				        // MOVE'
- *      goto DONE		            // JUMP'
- *    L2:                           // LABEL'
- *      r := FACT(n-1)	            // MOVE'
- *      a := n * r	                // MOVE'
- *      rv := a		                // MOVE'
- *      goto DONE	                // JUMP'
- *
- * The right-hand sides of register assignments & memory stores can be pure
- * expression trees or function calls. Funcall args must be pure expression
- * trees.
- *)
-
+// From an arbitrary Tree statement, produce a list of cleaned trees
+// satisfying the following properties:
+//    1. No SEQ's or ESEQ's
+//    2. The parent of every CALL is an EXP(..) or a MOVE(TEMP t, ..)
+//
+// A list of these things looks much more like "generic" assembler:
+//
+//    FACT:				            // LABEL
+//      if n=0 then L1 else L2		// CJUMP
+//    L1:				            // LABEL
+//      rv := 1				        // MOVE
+//      goto DONE		            // JUMP
+//    L2:                           // LABEL
+//      r := FACT(n-1)	            // MOVE
+//      a := n * r	                // MOVE
+//      rv := a		                // MOVE
+//      goto DONE	                // JUMP
+//
+// The right-hand sides of register assignments and memory stores can be pure
+// expression trees or function calls. Funcall args must be pure expression trees.
 let linearize stm0 =
 
     let nop = EXP (CONST 0)
 
+    // Eliminate useless NOP or unite in a SEQ
     let ( % ) x y =
         match x, y with
         | EXP (CONST _), _ -> y
         | _, EXP (CONST _) -> x
         | _                -> SEQ (x, y)
 
-    let rec stmtall f = function
-        | SEQ (a, b) -> stmtall f a && stmtall f b
+    // Apply given function starting from the last expression
+    let rec stmtSub f = function
+        | SEQ (a, b) -> stmtSub f a && stmtSub f b
         | s          -> f s
 
-    // 'stmt' is a list of Tree' statements.
-    // 'e' is a side-effect-free Tree' expression -- won't write a temp or
-    // memory, guaranteed to terminate.
+    // 'stmt' - a list of Tree statements.
+    // 'e'    - side-effect-free Tree expression -- won't write a temp or
+    //          memory and guaranteed to terminate.
     //
-    // - Return true if we are sure that (1) executing (stmt; exp) produces
-    //   the same value as (t := exp; stmt; t) for some fresh variable t.
-    // - Return false if the two executions would differ... or if we're not sure.
+    // - Return 'true' if we are sure that executing (stmt; exp) produces
+    //   the same value as (t := exp; stmt; t) for some fresh variable 't'.
+    // - Return 'false' if the two executions would differ... or if we're not sure.
     //
-    // Control effects: Note that if stmt contains a jump off to somewhere else,
+    // Control effects: Note that if 'stmt' contains a jump off to somewhere else,
     // neither expression produces any value at all! So they would commute. On
-    // the other hand, if stmt contains a jump outside of stmt, where we might
+    // the other hand, if 'stmt' contains a jump outside of 'stmt', where we might
     // execute arbitrary code, *then jump back*, said arbitrary code might write
-    // a location referenced by exp... so we throw up our hands and safely say
-    // false. You can't write a perfect commutation analysis, so don't lose sleep
-    // over it. This one is good enough to handle the way we yank calls up to top
-    // level, which is a desireable property.
+    // a location referenced by 'e'... so we throw up our hands and safely say 'false'.
     let rec commutes (stmt: Stm) (e: Exp) :bool =
         match stmt, e with
         | _, NAME _    -> true
         | _, CONST _   -> true
         | stmt, BINOP (_, e1, e2) -> commutes stmt e1 && commutes stmt e2
-        | stmt, TEMP t            -> stmtall (function
+        | stmt, TEMP t            -> stmtSub (function
                                                 | JUMP _            -> false
                                                 | CJUMP _           -> false
                                                 | EXP (CALL _)      -> true
@@ -74,7 +68,7 @@ let linearize stm0 =
                                                 | _                 -> failwithf "ERROR: Unexpected expression in `commute`."
                                              ) stmt
 
-        | stmt, MEM _             -> stmtall (function
+        | stmt, MEM _             -> stmtSub (function
                                                 | MOVE (MEM _, _)  -> false
                                                 | JUMP _           -> false
                                                 | CJUMP _          -> false
@@ -85,8 +79,8 @@ let linearize stm0 =
 
         | _ -> failwithf "ERROR: Unexpected statement in `commute`."
 
-    // Purify a list of N expressions -- boil out the side-effects into a
-    // stmt' list, and render the left-over, pure expressions as an N-elt exp' list.
+    // Boil out the side-effects into a Stm list, and render the left-over,
+    // pure expressions. (stmt; exp) to (t := exp; stmt; t) if commute.
     let rec doExps = function
         | [] -> (nop, [])
         | h::t -> let (stm1', e') = doExp h
@@ -98,7 +92,7 @@ let linearize stm0 =
                       let t = TEMP (Temp.newTemp())
                       (stm1' % MOVE (t, e') % stm2', t :: el)
 
-    // Turn a compex stm statement into a list of simple stm' statements.
+    // Turn a compex statement into a list of simple statements p. 176.
     and doStm = function
         | SEQ (a, b)     -> doStm a % doStm b
         | JUMP (e, labs) -> let (stm', e') = doExp e
@@ -127,8 +121,7 @@ let linearize stm0 =
         | LABEL _ as label -> label
         | _                -> failwithf "ERROR: Unexpected expression."
 
-    // Purify a complex expression. Boil out the side-effects into a stm' list,
-    // and render the left-over, pure expression as an exp'.
+    // Boil out the side-effects into a Stm list, and render the left-over, pure expression.
     and doExp = function
         | BINOP (p, a, b) -> match doExps [a; b] with
                              | stm', [a'; b'] -> (stm', BINOP (p, a', b'))
@@ -141,6 +134,7 @@ let linearize stm0 =
                              let (stm2', e') = doExp e
                              (stm1' % stm2', e')
 
+        // Moving calls to top level p. 178
         | CALL (f, args)  -> match doExps (f :: args) with
                              | stm', f' :: args' -> let t = TEMP (Temp.newTemp())
                                                     (stm' % (MOVE (t, CALL (f', args'))), t)
@@ -148,8 +142,8 @@ let linearize stm0 =
 
         | TEMP _ | CONST _ | NAME _ as other -> (nop, other)
 
-    // EXP is an expression that is the source of a MOVE statement. Boil out
-    // all of its side effects into a stm' list, and render the result as a suitable rval.
+    // Handle expression that is the source of a MOVE statement. Boil out all of its
+    // side effects into a Stm list, and render the result as a suitable return value.
     and doRVal = function
         | ESEQ (s, e)    -> let stm1' = doStm s
                             let (stm2', rv) = doRVal e
@@ -157,10 +151,11 @@ let linearize stm0 =
 
         | CALL (f, args) -> match doExps (f :: args) with
                             | stm', f' :: args' -> (stm', CALL (f', args'))
-                            | _                 -> failwithf "ERROR: Illegal parameter in right CALL."
+                            | _                 -> failwithf "ERROR: Illegal parameter in RHS CALL."
 
         | exp            -> doExp exp
 
+    // SEQ(s1, SEQ(s2, SEQ(sn-1, sn)...)) to [s1; s2; ..., sn-1, sn]
     let rec linear stm l =
         match stm with
         | SEQ (a, b) -> linear a (linear b l)
@@ -172,21 +167,30 @@ let linearize stm0 =
 
 type block = Stm list
 
+// From a list of cleaned trees, produce a list of basic blocks satisfying
+// the following properties:
+//     1. and 2. as above;
+//     3. Every block begins with a LABEL;
+//     4. A LABEL appears only at the beginning of a block;
+//     5. Any JUMP or CJUMP is the last stm in a block;
+//     6. Every block ends with a JUMP or CJUMP;
+//
+// Also produce the "label" to which control will be passed upon exit.
 let basicBlocks stms =
     let doneLab = Temp.newLabel()
 
     let rec blocks stms blist =
         match stms with
-        | LABEL _ as h :: l -> let rec next stms thisBlock =
+        | LABEL _ as h :: l -> let rec next stms block =
                                    match stms with
-                                   | JUMP _ as h :: l  -> endBlock l (h :: thisBlock)
-                                   | CJUMP _ as h :: l -> endBlock l (h :: thisBlock)
-                                   | LABEL lab :: _    -> next ((JUMP (NAME lab, [lab])) :: stms) thisBlock
-                                   | h :: l            -> next l (h :: thisBlock)
-                                   | []                -> next [JUMP (NAME doneLab, [doneLab])] thisBlock
+                                   | JUMP _ as h :: l  -> endBlock l (h :: block)
+                                   | CJUMP _ as h :: l -> endBlock l (h :: block)
+                                   | LABEL lab :: _    -> next ((JUMP (NAME lab, [lab])) :: stms) block
+                                   | h :: l            -> next l (h :: block)
+                                   | []                -> next [JUMP (NAME doneLab, [doneLab])] block
 
-                               and endBlock stms thisblock =
-                                    blocks stms (List.rev thisblock :: blist)
+                               and endBlock stms block =
+                                    blocks stms (List.rev block :: blist)
 
                                next l [h]
 
@@ -200,12 +204,14 @@ let enterBlock block table =
     | LABEL s :: _ -> Store.enter(table, s, block)
     | _            -> table
 
+// Convert list to ([1...n-1], n)
 let rec splitLast = function
     | [x]    -> ([], x)
     | h :: t -> let (t', last) = splitLast t
                 (h::t', last)
     | []     -> failwithf "ERROR: Can't split emtpy list."
 
+// Every CJUMP is followed by false lablel p. 181
 let rec trace table block rest =
     match block with
     | LABEL lab :: _ as b ->
@@ -241,6 +247,14 @@ and getNext table = function
   | [] -> []
   | _  -> failwithf "ERROR: Doesn't start with LABEL."
 
+// From a list of basic blocks satisfying properties 1-6, along with an "exit" label,
+// produce a list of stms such that:
+//     1. and 2. as above;
+//     7. Every CJUMP (_, t, f) is immediately followed by LABEL f.
+//
+// The blocks are reordered to satisfy property 7; also in this reordering
+// as many 'JUMP (NAME lab)' statements as possible are eliminated by falling
+// through into 'LABEL lab'.
 let traceSchedule (blocks, doneLab) =
     let table = List.foldBack enterBlock blocks Store.empty
     getNext table blocks @ [LABEL doneLab]
